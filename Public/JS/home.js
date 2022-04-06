8402f64f55fca876085222442f256b748bae9852
Pedro H Roquee <pedrokirk@msn.com>
2022-04-06 14:08:21 -0300

    corrigido

--- Public/JS/home.js
+++ Public/JS/home.js
@@ -1,7 +1,6 @@
 const myModal = new bootstrap.Modal("#transaction-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session")
-
 let data = {
     transactions: []
 };
